6c6a4de50b9b3a1136deae56e467bfa3d8f27831
Ian Nordby <ian.c.nordby@gmail.com>
2022-09-21 10:00:07 -0600

    Add spacing to read easier

--- concat_vs_inter.js
+++ concat_vs_inter.js
@@ -8,10 +8,12 @@ var stock = 23
 var distance = 3
 
 // using concatenation, we could:
+
 console.log(location + ' has ' + stock + ' ' + item + ' at $' + price + '. The closest ' + location + ' is ' + distance + ' miles away.')
 // I find this tedious and had to parse and construct.
 
 // using interpolation and template literals (` to the right of the 1 key), we can create a much more elegant version.
 // the ${} tells javascript to insert the value contained inside the {} into the `` template.
+
 console.log(`${location} has ${stock} ${item} at $${price}. The closest ${location} is ${distance} miles away.`)
 // This is much easier to parse, contruct and alter in my opinion.
